b86352fa16a24fd84be4cfb5a6c17d91af2040ed
NigelNunes <nigelnunes2004@gmail.com>
2024-11-13 14:47:26 +0530

    push

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,11 @@ const Footer = () => (
   <footer>
     <p>&copy; {new Date().getFullYear()} Nigel Nunes. All rights reserved.</p>
     <p>Contact me: nigelnunes2004@gmail.com</p>
+    <div class="resume-section">
+            <h3>Download My Resume</h3>
+            <a href="Nigel Resume.pdf" class="resume-button" target="_blank" download="Nigel Resume.pdf">Download Resume</a>
+            
+        </div>
   </footer>
 );
 
